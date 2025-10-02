83b13a43465fdc5fc9d53c2dde2a0e38d0193c2a
Michel Siegert <michel@siegert.online>
2025-10-02 21:38:26 +0200

    adding map to application.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,14 @@
 import { Component, signal } from '@angular/core';
+import { MapComponent } from './map/map';
+import { HttpClientModule } from '@angular/common/http';
+import { Marker } from './marker';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [MapComponent, HttpClientModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  providers:[Marker]
 })
 export class App {
   protected readonly title = signal('angular-leaflet-example');
