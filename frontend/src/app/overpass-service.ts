5cbf008659b3d8f45175cce9ac2372f7685d305c
Michel Siegert <michel@siegert.online>
2026-01-01 18:27:04 +0100

    Fixing a bug where you couldnt filter by branche

--- frontend/src/app/overpass-service.ts
+++ frontend/src/app/overpass-service.ts
@@ -1,20 +1,26 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
+import { BranchService } from './branch-service';
 
 @Injectable({providedIn: 'root'})
 export class overpassService {
   private overpassUrl = 'https://overpass-api.de/api/interpreter';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private branchService: BranchService) {}
+  
 
-  getNearbyHandwerker(lat: number, lon: number, radius = 5000): Observable<any> {
+  async getNearbyCompanies(lat: number, lon: number, radius = 5000): Promise<Observable<any>> {
+    const branch = await firstValueFrom(this.branchService.currentBranch);
+
+    const branchFilter = branch ? `["craft"="${branch}"]` : '["craft"]';
+
     const query = `
       [out:json][timeout:25];
       (
-        node["craft"](around:${radius},${lat},${lon});
-        way["craft"](around:${radius},${lat},${lon});
-        relation["craft"](around:${radius},${lat},${lon});
+        node${branchFilter}(around:${radius},${lat},${lon});
+        way${branchFilter}(around:${radius},${lat},${lon});
+        relation${branchFilter}(around:${radius},${lat},${lon});
       );
       out center tags;
     `;
